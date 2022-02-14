bdfc25b167018a35451bbd48dafa3711da90fda2
Nolan Caine <nolancaine@gmail.com>
2022-02-14 15:15:33 -0700

    Wrap app in Apollo provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,19 @@
 import '../styles/globals.css';
 
 import type { AppProps } from 'next/app';
+import { FC } from 'react';
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+import { ApolloProvider } from '../lib/apollo-client';
+
+const App: FC<AppProps> = ({
+  Component,
+  pageProps: { initialApolloState, ...pageProps },
+}: AppProps) => {
+  return (
+    <ApolloProvider initialState={initialApolloState || {}}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 };
 
-export default MyApp;
+export default App;
